8a291d3aff6dd2ab53ac515180e98d85a776f3d8
logue <logue@hotmail.co.jp>
2020-11-20 16:25:57 +0900

    Fixed a bug that caused a no-unpublished-import error when installing Bootstrap-vue or Vuetiry.

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'node/no-unsupported-features/es-syntax': [
-      'error',
-      {
-        ignores: ['modules'],
-      },
-    ],
     'node/no-missing-import': 'off',
+    'node/no-unpublished-import': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
   },
 };
