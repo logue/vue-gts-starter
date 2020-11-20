764b577c562d966b07c9cb0b28cd816ee6150928
logue <logue@hotmail.co.jp>
2020-11-20 13:19:57 +0900

    Based on the rules defined on the GTS side. For this reason, the libraries included in GTS have been removed from the project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,27 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    './node_modules/gts',
+    'plugin:vue/essential',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      {
+        ignores: ['modules'],
+      },
+    ],
+    'node/no-missing-import': 'off',
+  },
 };
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+module.exports = {
+  ...require('./node_modules/gts/.prettierrc.json'),
+  htmlWhitespaceSensitivity: 'ignore',
+  bracketSpacing: true,
+  printWidth: 80,
+};
