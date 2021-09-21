330a76d522f9ab49a94a60d8d22479b7ca740021
Logue <logue@hotmail.co.jp>
2021-09-22 00:01:55 +0900

    Fixed typo. Modify Rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,38 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   env: {
     browser: true,
     es6: true,
-    node: true,
   },
   extends: [
-    './node_modules/gts',
     'eslint:recommended',
+    './node_modules/gts',
+    'plugin:vue/recommended',
+    '@vue/typescript',
     '@vue/typescript/recommended',
+    'prettier',
     '@vue/prettier',
-    'plugin:vue/essential',
     '@vue/prettier/@typescript-eslint',
   ],
-  plugins: ['vue', 'tsdoc', 'prettier'],
+  plugins: [
+    'vue',
+    'prettier',
+    '@typescript-eslint/eslint-plugin',
+    'eslint-plugin-tsdoc',
+  ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
+    sourceType: 'module',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'node/no-extraneous-import': 'off',
     'node/no-missing-import': 'off',
     'node/no-unpublished-import': 'off',
+    'node/no-unpublished-require': 'off',
     'node/no-unsupported-features/es-syntax': 'off',
   },
 };
